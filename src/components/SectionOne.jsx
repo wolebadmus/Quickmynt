653de2b2e4aa964365888a600c5ab7ce73e1dac7
Mofiyinfoluwa Praise Ayanwale <aypraisem@gmail.com>
2024-10-22 16:53:45 +0100

    Header andd section-one fixes

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -1,6 +1,6 @@
 const SectionOne = () => {
     return (
-        <section className={"banner flex flex-col w-full h-[42em] lg:h-[69.8125em] lg:max-h-[100vh] pt-[8em] bg-right-bottom lg:bg-center relative justify-center items-center gap-6"}>
+        <section className={"banner flex flex-col w-full h-[42em] lg:h-[69.8125em] lg:max-h-[100vh] pt-[8em] bg-right-bottom lg:bg-center relative justify-start items-center gap-6"}>
             <div className="absolute w-full h-full bg-custom-gradient mix-blend-multiply -mt-[8em]"></div>
             <div className="flex flex-col justify-center gap-6 lg:gap-3 lg:w-full lg:max-w-[71.25em]">
                 <div className="z-40 flex flex-col justify-evenly gap-4 lg:gap-0 h-[18.075em] lg:h-[39.824em] max-w-[20.9375em] lg:max-w-[33.563em] ">
