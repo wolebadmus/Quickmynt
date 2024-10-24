7a8d5c9dd3c1fa73e4ddb157677ede210423cbd4
Mofiyinfoluwa Praise Ayanwale <aypraisem@gmail.com>
2024-10-24 16:30:06 +0100

    Contact Us Page

--- src/components/HomePageComponents/SectionOne.jsx
+++ src/components/HomePageComponents/SectionOne.jsx
@@ -6,7 +6,7 @@ const SectionOne = () => {
             <div className="absolute w-full h-full bg-custom-gradient mix-blend-multiply -mt-[2.8em]"></div>
             <div className="flex flex-col justify-center gap-6 lg:gap-5 lg:w-full lg:max-w-[90em] h-[22.249em] lg:h-[39.824em]">
                 <div className="z-40 flex flex-col justify-center gap-4 lg:gap-6 max-w-[20.9375em] lg:max-w-[33.563em]  ">
-                    <p className="lg:text-base text-[#F3FEF6] text-[3.642em] lg:text-[4.61em] font-osande font-[600] lg:font-[800] leading-[60.48px] lg:leading-[88px] text-center lg:text-left">
+                    <p className="text-[#F3FEF6] text-[3.642em] lg:text-[4.61em] font-osande font-[600] lg:font-[800] leading-[60.48px] lg:leading-[88px] text-center lg:text-left">
                         Turning Every Workday into Payday.
                     </p>
                     <p className="hidden lg:block text-[#FDFDFD] text-[1.6769em] font-osande font-[600] leading-[32.71px] text-left">
@@ -16,7 +16,7 @@ const SectionOne = () => {
                         QuickMynt delivers a cutting-edge earned wage access platform, giving employees the freedom to access their pay on their terms, with no disruptions to payroll.
                     </p>
                 </div>
-                <button className="z-40 lg:text-base text-[#F3FEF6] font-graphik font-[500] text-[1.0112em] lg:text-[1.094em] leading-[17.8px] lg:leading-[23.05px]
+                <button className="z-40 text-[#F3FEF6] font-graphik font-[500] text-[1.0112em] lg:text-[1.094em] leading-[17.8px] lg:leading-[23.05px]
                  bg-[#488559] w-[11.6669em] lg:w-[10.5981em] h-[2.3887em] lg:h-[3.0744em] hover:text-[#488559]
                   hover:bg-[#FFF] hover:border hover:border-[#488559] rounded-full z-40 mx-auto lg:mx-0">
                     Request a Demo
